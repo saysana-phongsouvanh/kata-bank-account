3948301dfd932d4b2143032a746bdd563b1e392a
Saysana PHONGSOUVANH <saysana.phongsouvanh@carbon-it.com>
2022-10-27 09:53:26 +0200

    fix(US-3): fix date in Formatter tests

--- src/test/java/io/phongsouvanh/bank/statements/StatementGridFormatterTest.java
+++ src/test/java/io/phongsouvanh/bank/statements/StatementGridFormatterTest.java
@@ -12,6 +12,8 @@ import java.time.Clock;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
@@ -22,6 +24,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class StatementGridFormatterTest {
 
     private final Clock clock = Clock.fixed(Instant.parse("2022-10-25T15:30:00Z"), ZoneId.of("UTC"));
+    private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     private StatementGridFormatter statementGridFormatter;
 
@@ -34,16 +37,17 @@ class StatementGridFormatterTest {
     @DisplayName("should format statement with operations")
     void shouldFormatStatementWithOperations() throws Exception {
         final UUID accountId = UUID.randomUUID();
+        final LocalDateTime date = LocalDateTime.now(clock);
         final List<Operation> operations = Arrays.asList(
             new Operation(accountId, OperationType.DEPOSIT, new Amount(new BigDecimal(10000)), LocalDateTime.now(clock), new BigDecimal(10000)),
             new Operation(accountId, OperationType.WITHDRAWAL, new Amount(new BigDecimal(100)), LocalDateTime.now(clock).plusMinutes(5), new BigDecimal(9900)),
             new Operation(accountId, OperationType.WITHDRAWAL, new Amount(new BigDecimal(7000)), LocalDateTime.now(clock).plusMinutes(6), new BigDecimal(2900))
         );
-        final Statement statement = new Statement(accountId, LocalDateTime.now(), operations);
+        final Statement statement = new Statement(accountId, LocalDateTime.now(clock), operations);
         final String expectedStatement = """
                 +------------------------------------------------------------------------------------------------------+
                 |                                                                                                      |
-                |                                   ACCOUNT STATEMENT OF 2022-10-26                                    |
+                |                                   ACCOUNT STATEMENT OF %s                                    |
                 |                                                                                                      |
                 +------------------------------------------------------------------------------------------------------+
                 |                                           ACCOUNT DETAILS                                            |
@@ -57,9 +61,14 @@ class StatementGridFormatterTest {
                 |  WITHDRAWAL      2022-10-25 15:36:00       -7000.00€                 2900.00€                        |
                 |  WITHDRAWAL      2022-10-25 15:35:00       -100.00€                  9900.00€                        |
                 |  DEPOSIT         2022-10-25 15:30:00       +10000.00€                10000.00€                       |
-                +------------------------------------------------------------------------------------------------------+""".formatted(accountId);
+                +------------------------------------------------------------------------------------------------------+"""
+                .formatted(date.format(dateFormatter), accountId);
 
-        final String resultStatement = statementGridFormatter.format(statement).stream().map(n -> String.valueOf(n)).collect(Collectors.joining("\n"));
+        final String resultStatement = statementGridFormatter
+                .format(statement)
+                .stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining("\n"));
         assertEquals(expectedStatement, resultStatement);
     }
 
@@ -67,12 +76,13 @@ class StatementGridFormatterTest {
     @DisplayName("should format statement without operations")
     void shouldFormatStatementWithoutOperations() {
         final UUID accountId = UUID.randomUUID();
-        final List<Operation> operations = Arrays.asList();
-        final Statement statement = new Statement(accountId, LocalDateTime.now(), operations);
+        final LocalDateTime date = LocalDateTime.now(clock);
+        final List<Operation> operations = new ArrayList<>();
+        final Statement statement = new Statement(accountId, LocalDateTime.now(clock), operations);
         final String expectedStatement = """
                 +------------------------------------------------------------------------------------------------------+
                 |                                                                                                      |
-                |                                   ACCOUNT STATEMENT OF 2022-10-26                                    |
+                |                                   ACCOUNT STATEMENT OF %s                                    |
                 |                                                                                                      |
                 +------------------------------------------------------------------------------------------------------+
                 |                                           ACCOUNT DETAILS                                            |
@@ -84,9 +94,14 @@ class StatementGridFormatterTest {
                 |  TYPE            DATE                      AMOUNT                    BALANCE                         |
                 +------------------------------------------------------------------------------------------------------+
                 |  No operations were found for this account                                                           |
-                +------------------------------------------------------------------------------------------------------+""".formatted(accountId);
+                +------------------------------------------------------------------------------------------------------+"""
+                .formatted(date.format(dateFormatter), accountId);
 
-        final String resultStatement = statementGridFormatter.format(statement).stream().map(n -> String.valueOf(n)).collect(Collectors.joining("\n"));
+        final String resultStatement = statementGridFormatter
+                .format(statement)
+                .stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining("\n"));
         assertEquals(expectedStatement, resultStatement);
     }
 
